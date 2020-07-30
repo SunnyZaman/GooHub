1b56ef10d43eea47537a1cea0a4ddcb8e6ad515b
Zaman <sjzaman@ryerson.ca>
2020-07-29 23:30:18 -0400

    styled components

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const FooterWrapper = styled.div`
+    position: absolute;
+    bottom: 0;
+    width:100%
+`;
+function Footer() {
+    return (
+        <FooterWrapper>
+            <h1>Footer</h1>
+        </FooterWrapper>
+    );
+}
+
+export default Footer;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import Routes from './router';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import styled from 'styled-components';
 import * as serviceWorker from './serviceWorker';
+import GlobalStyle from './globalStyle';
+import { HashRouter } from 'react-router-dom';
+
+const MainWrapper = styled.div`
+  flex-grow: 1;
+  position: relative;
+  min-height: 100vh
+`;
+const AppContainer = styled.div`
+  padding-bottom: 0
+`;
+const ViewsContainer = styled.div`
+  width:100%
+`;
 
 ReactDOM.render(
   <React.StrictMode>
+    <HashRouter>
+      <GlobalStyle />
+      <MainWrapper>
+        <AppContainer>
+          <Header />
+          <ViewsContainer>
+            <Routes />
+          </ViewsContainer>
+        </AppContainer>
+        <Footer />
+      </MainWrapper>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
