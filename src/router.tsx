4a4887b4bf081d6f5e706f121e93e26d9f397696
Zaman <sjzaman@ryerson.ca>
2020-07-29 22:11:18 -0400

    routes

---
+++ src/router.tsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Homepage from './views/Homepage';
+import Profile from './views/Profile';
+function Routes() {
+    return (
+        <Switch>
+            <Route exact path="/" component={Homepage} />
+            <Route exact path="/profile/:requestId?" component={Profile} />
+            <Redirect to="/" />
+        </Switch>
+    )
+}
+export default Routes;
