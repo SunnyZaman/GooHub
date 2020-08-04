e8373f8f1b9888031e689cba0e25b56ff8d99052
Zaman <sjzaman@ryerson.ca>
2020-08-03 23:13:57 -0400

    Added closed icon

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -2,4 +2,6 @@
 export const GoogleLogo:string = require('./Google_logo.svg');
 export const GLogo:string = require('./G_logo.svg');
 export const SearchIcon:string = require('./icons/search.svg');
+export const MicIcon:string = require('./icons/Google_mic.svg');
+export const CloseIcon:string = require('./icons/close.svg');
 
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { SearchIcon } from '../assets/images';
+import { SearchIcon, MicIcon, CloseIcon } from '../assets/images';
 const SearchWrapper = styled.div`
     position: relative;
 
@@ -20,18 +20,41 @@ const SearchInput = styled.input`
 
 const SearchImage = styled.img`
   position: absolute;
-  left: 20;
+  left: 20px;
   top: 15px;
   padding: 9px 8px;
-  color: #aaa;
-  transition: 0.3s;
+  filter: invert(73%) sepia(8%) saturate(262%) hue-rotate(169deg) brightness(87%) contrast(85%);
+`;
+
+const MicImage = styled.img`
+  position: absolute;
+  right: 20px;
+  top: 10px;
+  padding: 9px 8px;
+  `;
+
+const CloseImage = styled.img`
+    position: absolute;
+    right: 48px;
+    border-left:1px solid #80868b;
+    top: 13px;
+    padding: 9px 8px;
+    filter: invert(58%) sepia(10%) saturate(213%) hue-rotate(165deg) brightness(88%) contrast(87%);
 `;
 
 function SearchBar() {
+    const [hasInput, setHasInput] = useState(false);
+    const handleChange = (event: any) => {
+        const { name, value } = event.target;
+        console.log(name, value);
+        setHasInput(value.length > 0);
+    }
     return (
         <SearchWrapper>
-            <SearchInput type="text"></SearchInput>
-            <SearchImage className="" src={SearchIcon}  alt="Search Icon" height="15px" width="auto"/>
+            <SearchInput type="text" name="search" onChange={handleChange}></SearchInput>
+            <SearchImage src={SearchIcon} alt="Search Icon" height="15px" width="auto" />
+            {hasInput ? <CloseImage src={CloseIcon} tabIndex={0} alt="Close Icon" height="15px" width="auto" /> : null}
+            <MicImage src={MicIcon} tabIndex={0} alt="Mic Icon" height="20px" width="auto" />
         </SearchWrapper>
     );
 }
