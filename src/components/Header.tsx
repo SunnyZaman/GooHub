8160c47fa2e952c5e61fbbbec941a3a26546c7e6
Zaman <sjzaman@ryerson.ca>
2020-08-09 14:16:45 -0400

    Added link to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,20 +17,27 @@ const InnerContainer = styled.div`
     margin: 0 auto;
     display: flex;
     flex-direction: row;
-     flex-wrap: wrap;
+    flex-wrap: wrap;
     justify-content: space-between;
     width: 100%;
-    max-width: 148rem
+    max-width: 148rem;
+    padding: 0 15px;
 `;
 const Link = styled.a`
-    font-size: 13px
+    font-size: 13px;
+    color:#595959;
+    text-decoration: none;
+    &:hover{
+        text-decoration: underline;
+    }
 `;
 
 function Header() {
     return (
         <Wrapper>
          <InnerContainer>
-            <Link>Header</Link>
+             {/* temp link, will link to my portfolio */}
+            <Link href="https://github.com/SunnyZaman" target="_blank">Sunny Zaman</Link>
             <img src={GLogo}  alt="G logo" height="20px" width="auto"/>
          </InnerContainer>
 
