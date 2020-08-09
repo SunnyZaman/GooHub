84a64792a5242e2a2fb6e0f42e20f8d6647be87b
Zaman <sjzaman@ryerson.ca>
2020-08-09 14:09:23 -0400

    Added text to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,12 +4,24 @@ import styled from 'styled-components';
 const FooterWrapper = styled.div`
     position: absolute;
     bottom: 0px;
-    width:100%
+    width:100%;
+    color: rgba(0,0,0,.54);
+    background: #f2f2f2;
+    border-top: 1px solid #e4e4e4;
+`;
+const TextWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    padding: 0 15px;
+    font-size: 15px;
 `;
 function Footer() {
     return (
         <FooterWrapper>
-            <h1>Footer</h1>
+            <TextWrapper>
+            <p>Canada</p>
+            <p>2020</p>
+            </TextWrapper>
         </FooterWrapper>
     );
 }
